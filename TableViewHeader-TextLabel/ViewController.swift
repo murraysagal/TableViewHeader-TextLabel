d6bd33dfa6542e06fa8fb02d37630713b5dfef79
murraysagal <murraysagal@mac.com>
2019-03-07 14:01:57 -0600

    initial commit

---
+++ TableViewHeader-TextLabel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewHeader-TextLabel
+//
+//  Created by Murray Sagal on 2019-03-07.
+//  Copyright © 2019 Murray Sagal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
